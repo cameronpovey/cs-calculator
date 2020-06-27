1840eecd49bacca1b350bdd99ee5a1e6a4c16f84
cameronpovey <66805733+cameronpovey@users.noreply.github.com>
2020-06-27 14:03:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ namespace Calculator
 
                 do
                 {
+                    //ask for correct function on repeat untill correct function is entered
                     fneg = false;
                     Console.WriteLine("(*, /, -, + or =) Enter Function " + count + ": ");
                     c = Console.ReadLine();
@@ -34,20 +35,24 @@ namespace Calculator
 
                     if (c != "*" && c != "/" && c != "-" && c != "+" && c != "=")
                     {
+                        //check for valid functions
                         Console.WriteLine("Enter a valid function - *, /, -, + or =");
                         fneg = true;
                     }
                     else if (count == 1 && c == "=")
                     {
+                        //ensure = isnt entered on first loop
                         Console.WriteLine("Enter a valid sum with atleast one function.");
                         fneg = true;
                     }
                     else if (c == "=")
                     {
+                        //when equals is enter combine the list of nums and func
                         pass = -1;
                         foreach (int x in nums)
                         {
                             if (pass == -1) { total = x; }
+                            //checks for each function and executes action
                             else if (func[pass] == "*") { total *= x; }
                             else if (func[pass] == "/") { total /= x; }
                             else if (func[pass] == "-") { total -= x; }
