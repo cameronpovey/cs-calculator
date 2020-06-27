5ebe6337eebf0f5c81664ec4c132f11b8b34dd9d
cameronpovey <66805733+cameronpovey@users.noreply.github.com>
2020-06-27 13:44:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set varibles
+            bool exit = false;
+            bool fneg = false;
+            int total = 0;
+            int pass = 0;
+            int count = 1;
+            string c = "";
+
+            //setup lists for numbers and functions
+            List<int> nums = new List<int>();
+            List<string> func = new List<string>();
+
+            while (exit == false)
+            {
+                //add number one by one onto nums list
+                Console.WriteLine("Enter number " + count + ": ");
+                nums.Add(Convert.ToInt32(Console.ReadLine()));
+
+                do
+                {
+                    fneg = false;
+                    Console.WriteLine("(*, /, -, + or =) Enter Function " + count + ": ");
+                    c = Console.ReadLine();
+                    
+
+                    if (c != "*" && c != "/" && c != "-" && c != "+" && c != "=")
+                    {
+                        Console.WriteLine("Enter a valid function - *, /, -, + or =");
+                        fneg = true;
+                    }
+                    else if (count == 1 && c == "=")
+                    {
+                        Console.WriteLine("Enter a valid sum with atleast one function.");
+                        fneg = true;
+                    }
+                    else if (c == "=")
+                    {
+                        pass = -1;
+                        foreach (int x in nums)
+                        {
+                            if (pass == -1) { total = x; }
+                            else if (func[pass] == "*") { total *= x; }
+                            else if (func[pass] == "/") { total /= x; }
+                            else if (func[pass] == "-") { total -= x; }
+                            else if (func[pass] == "+") { total += x; }
+                            pass++;
+                        }
+                        Console.WriteLine(total);
+                        exit = true;
+                    }
+                }
+                while (fneg == true);
+
+                func.Add(c);
+
+                count++;
+            }
+        }
+    }
+}
